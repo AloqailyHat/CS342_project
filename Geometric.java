04400a527bf3f93bc1e1a13fd9050a64d3aae6f0
hatoon-fah <66895543+hatoon-fah@users.noreply.github.com>
2022-10-16 03:02:36 +0300

    start

---
+++ Geometric.java
@@ -0,0 +1,22 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Geometric  {
+   private JLabel label1, label2;
+   private JButton b1, b2;
+   private JTextField t1, t2;
+   
+   public Geometric(){
+     
+       
+   }
+ 
+
+    public static void main(String[] args) {
+        Geometric w= new Geometric();
+    }
+
+
+}
